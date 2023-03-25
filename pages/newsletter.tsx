264686012e4d0f2f028ef97bfe4a3f1c31df8eba
felipemarinhodev <felipmarinho@gmail.com>
2023-03-24 22:52:28 -0700

    a1.5 - create logic to form

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -3,42 +3,83 @@ import Button from "@src/components/Button/Button";
 import Image from "@src/components/Image/Image";
 import Text from "@src/components/Text/Text";
 import { BaseComponent } from "@src/theme/BaseComponent";
+import { useState } from "react";
+
+function useForm({ initialValues }) {
+  const [values, setValues] = useState(initialValues);
+
+  return {
+    values, handleChange(event) {
+      const { name, value } = event.target;
+      setValues(oldValue => ({
+        ...oldValue,
+        [name]: value
+      }))
+    }
+  }
+}
 
 export default function NewsletterScreen() {
+  const form = useForm({
+    initialValues: {
+      emailNewsletter: ""
+    }
+  });
+
   return (<Box
     styleSheet={{
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center'
     }}>
-    <Box
-      styleSheet={{
-        alignItems: 'center',
-        width: "100%",
-        maxWidth: "400px",
-        padding: '16px'
-      }}
-    >
-      <Image
-        src="https://github.com/felipemarinhodev.png"
-        alt="Foto do FelipeMarinhoDev"
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      console.log("Estamos enviando os dados do formulário!");
+
+      if (!form.values.emailNewsletter.includes("@")) {
+        alert("Você precisa informar um email valido");
+        return;
+      }
+
+      alert("Você foi cadastrado com sucesso! Cheque o seu mail para garantir!")
+    }}>
+      <Box
         styleSheet={{
-          borderRadius: "100%",
-          width: "100px",
-          marginBottom: "16px"
+          alignItems: 'center',
+          width: "100%",
+          maxWidth: "400px",
+          padding: '16px'
         }}
-      />
-      <Text variant="heading3">
-        Newsletter do FelipeMarinhoDev
-      </Text>
-      <NewsletterTextField placeholder="Informe o seu email" />
-      <Button fullWidth styleSheet={{ marginTop: "16px" }}>Cadastrar </Button>
-    </Box>
+      >
+        <Image
+          src="https://github.com/felipemarinhodev.png"
+          alt="Foto do FelipeMarinhoDev"
+          styleSheet={{
+            borderRadius: "100%",
+            width: "100px",
+            marginBottom: "16px"
+          }}
+        />
+        <Text variant="heading3">
+          Newsletter do FelipeMarinhoDev
+        </Text>
+        <NewsletterTextField
+          placeholder="Informe o seu email"
+          name="emailNewsletter"
+          value={form.values.emailNewsletter}
+          onChange={(form.handleChange)}
+        />
+        <Button fullWidth styleSheet={{ marginTop: "16px" }}>Cadastrar </Button>
+      </Box>
+    </form>
   </Box>)
 }
 
 interface NewsletterTextFieldProps {
   placeholder?: string
+  value?: string
+  name: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 function NewsletterTextField(props: NewsletterTextFieldProps) {
